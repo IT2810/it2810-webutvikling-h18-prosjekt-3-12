68765359f640a2731c6562ff2be9bac70f1ab865
ulvestad <simenu95@gmail.com>
2018-10-17 14:49:34 +0200

    prelimenary snapshot testing

--- pim/__tests__/Steps-test.js
+++ pim/__tests__/Steps-test.js
@@ -2,18 +2,25 @@ import "react-native";
 import React from "react";
 import Steps from "../screens/StepsScreen";
 import renderer from "react-test-renderer";
+import ShallowRenderer from "react-test-renderer/shallow";
 
-it("should render steps view correctly", () => {
-  //tests a snapshot view
-  const tree = renderer.create(<Steps />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe("Steps snapshot", () => {
+  it("should render steps view correctly", () => {
+    const renderer = new ShallowRenderer();
+    const result = renderer.render(<Steps />);
+    console.log(result);
+    expect(result).toMatchSnapshot();
+  });
+
+  it("should render outer most view in steps view correctly", () => {
+    //tests a snapshot view
+    const tree = renderer.create(<Steps />).toJSON();
+    console.log(tree);
+    expect(tree).toMatchSnapshot();
+  });
 });
 
 //tests component functions
 let stepsContainer = renderer.create(<Steps />).getInstance();
 let formatedDate = stepsContainer.formatDate();
 console.log(formatedDate);
-
-it('formatDate returns date on format "Oct 2, 17:32"', () => {
-  expect(formatedDate).stringMatching(/[a-zA-Z]+/);
-});

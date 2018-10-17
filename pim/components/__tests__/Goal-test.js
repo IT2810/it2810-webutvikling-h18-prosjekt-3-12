33638c28d1a0ba7f9f2da34aa9cf5a867eee0df0
ulvestad <simenu95@gmail.com>
2018-10-17 16:02:16 +0200

    tweaks for goal-test.js

--- pim/components/__tests__/Goal-test.js
+++ pim/components/__tests__/Goal-test.js
@@ -1,23 +1,31 @@
-import 'react-native';
-import React from 'react';
-import Goal from "../Goal.js"
-import renderer from 'react-test-renderer';
-
-jest.useFakeTimers();
-  beforeEach(() => {
-
-  });
-
+import "react-native";
+import React from "react";
+import Goal from "../Goal.js";
+import renderer from "react-test-renderer";
+import ShallowRenderer from "react-test-renderer/shallow";
 
 describe("Goal snapshot", () => {
-    it('It should render Goal component correctly', () => {
-        const tree = renderer.create(<Goal
-            asyncKey={"1"}
-            name={"2"}
-            desc={"3"}
-            date={"4"}
-            numGoals={5}/>).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
-});
+  jest.useFakeTimers();
+  beforeEach(() => {});
 
+  const goal = (
+    <Goal
+      asyncKey={"10394234234"}
+      name={"Løp 1000m"}
+      desc={"Mot opp ved lerkendal stadion og løp med Anders"}
+      date={"Oct 3, 11:32"}
+      numGoals={5}
+    />
+  );
+
+  it("It should render the whole Goal component correctly", () => {
+    const tree = renderer.create(goal).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("should shallowrender Goal component correctly", () => {
+    const renderer = new ShallowRenderer();
+    const result = renderer.render(goal);
+    expect(result).toMatchSnapshot();
+  });
+});
